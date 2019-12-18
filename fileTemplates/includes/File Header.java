3fa4f9798fa5ea5506511e3751f1ec48da7ee2cd
guojianWang <1085844536@qq.com>
2019-12-18 16:03:12 +0800

    IU-2018.3.5 <Y50c@Lenovo-PC Create markdown.xml, project.default.xml, diff.xml, yiiguxing.translation.xml, Default.xml, Nice Python.icls, find.xml, File Header.java, nginx.xml, _@user_Darcula.icls, mavenVersion.xml, GrepConsole.xml, Default.xml, Eclipse copy.xml, editor.codeinsight.xml, editor.xml, uml.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, CodeGlance.xml, rainbow_brackets.xml, lombok-plugin.xml, other.xml, ActionScript File Header.as, ui.lnf.xml, gradle.settings.xml, filetypes.xml, javaeeExternalResources.xml, webDeployTransportHostTargets.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, Sublime Monokai True.icls, laf.xml, material_custom_theme.xml, jdk.table.xml, path.macros.xml, Jsp File.jsp, intentionSettings.xml, material_theme.xml, proxy.settings.xml, vcs.xml, codota.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,25 @@
+/**
+@author Guojian Wang
+@date ${DATE} - ${TIME}
+@version 1.0
+@since   1.0
+ ━━━━━━神兽出没━━━━━━
+ 　　　┏┓　　　┏┓
+ 　　┏┛┻━━━┛┻┓
+ 　　┃　　　　　　　┃
+ 　　┃　　　━　　　┃
+ 　　┃　┳┛　┗┳　┃
+ 　　┃　　　　　　　┃
+ 　　┃　　　┻　　　┃
+ 　　┃　　　　　　　┃
+ 　　┗━┓　　　┏━┛
+ 　　　　┃　　　┃神兽保佑, 永无BUG!
+ 　　　　 ┃　　　┃Code is far away from bug with the animal protecting
+ 　　　　┃　　　┗━━━┓
+ 　　　　┃　　　　　　　┣┓
+ 　　　　┃　　　　　　　┏┛
+ 　　　　┗┓┓┏━┳┓┏┛
+ 　　　　　┃┫┫　┃┫┫
+ 　　　　　┗┻┛　┗┻┛
+ ━━━━━━感觉萌萌哒━━━━━━
+*/
